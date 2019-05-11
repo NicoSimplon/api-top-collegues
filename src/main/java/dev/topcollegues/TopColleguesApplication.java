02097999d153d42eec9aa0e41e152c067aa37d42
Nicolas <33360039+NicoSimplon@users.noreply.github.com>
2019-05-12 00:57:06 +0200

    Premier commit avec la base SpringBoot de l'application

---
+++ src/main/java/dev/topcollegues/TopColleguesApplication.java
@@ -0,0 +1,13 @@
+package dev.topcollegues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopColleguesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopColleguesApplication.class, args);
+	}
+
+}
